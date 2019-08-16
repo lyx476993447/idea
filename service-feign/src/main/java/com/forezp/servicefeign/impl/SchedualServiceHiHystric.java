9c3ebc103763f78e1f569b022484782ca87ec6f3
seriel <1679783299@qq.com>
2019-08-16 10:47:53 +0800

    forth commit zuul

---
+++ service-feign/src/main/java/com/forezp/servicefeign/impl/SchedualServiceHiHystric.java
@@ -0,0 +1,12 @@
+package com.forezp.servicefeign.impl;
+
+import com.forezp.servicefeign.Interface.SchedualServiceHi;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SchedualServiceHiHystric implements SchedualServiceHi {
+    @Override
+    public String sayHiFromClientOne(String name) {
+        return "sorry "+name;
+    }
+}
